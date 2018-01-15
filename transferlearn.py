c2cfbcb9601d370f816212095f22fdfa6bf81b53
snigdha <snigdha.snigz@gmail.com>
2018-01-16 03:24:41 +0530

    function for training the model

--- transferlearn.py
+++ transferlearn.py
@@ -75,9 +75,79 @@ out = torchvision.utils.make_grid(inputs)
 
 imshow(out, title=[class_names[x] for x in classes])
 
+# general function to train the model
 
+def train_model(model, criterion, optimizer, scheduler, num_epochs = 25):
+    since = time.time()
+    
+    best_model_wts = model.state_dict()
+    best_acc = 0.0
+    
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs-1))
+        print('-'*10)
+        
+        # each epoch has a training and a validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)  # set model to training mode
+            else:
+                model.train(False) # set model to validate(evaluate) mode
+            
+            running_loss = 0.0
+            running_corrects = 0
+            
+            # iterate over data.
+            for data in dataloaders[phase]:
+                # get the inputs
+                inputs, labels = data
+                
+                # wrap them in Variable
+                if use_gpu:
+                    inputs = Variable(inputs.cuda())
+                    labels = Variable(labels.cuda())
+                else:
+                    inputs, labels = Variable(inputs), Variable(labels)
+                    
+                
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+                
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+                    
+                # statistics
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(preds == labels.data)
+                
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+            
+            print('{} loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            
+            
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = model.state_dict()
+        
+        print()
 
-
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    
+    #load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
 
 
 
